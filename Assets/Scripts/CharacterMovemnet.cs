e3bb3c3c62389b5087b661f6817c29752fe5e3cb
The_Cleric_ <thecleric00@gmail.com>
2021-01-29 22:18:26 +1000

    Revert "Merge branch 'main' of https://github.com/Epidemical/GGJ2021 into main"
    This reverts commit eabe9ce5d9cbe52210e49b45d42f515c46043d54, reversing
    changes made to b8061ef5328d924b0fb4938fe3a69b8acce3b226.

--- Assets/Scripts/CharacterMovemnet.cs
+++ Assets/Scripts/CharacterMovemnet.cs
@@ -6,37 +6,16 @@ public class CharacterMovemnet : MonoBehaviour
 {
 
     public CharacterController controller;
-    public Transform groundCheck;
-    public float groundDistance = 0.4f;
-    public LayerMask groundMask;
-
-    public float speed = 7f;
-    public float gravity = -9.81f;
-
-    Vector3 velocity;
-    bool isGrounded;
-
-
+    public float speed = 5f;
 
     // Update is called once per frame
     void Update()
     {
-        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
-
-        if(isGrounded && velocity.y < 0)
-        {
-            velocity.y = -2f;
-        }
-
-        float horizontal = Input.GetAxisRaw("Horizontal"); //A AND D
-        float vertical = Input.GetAxisRaw("Vertical"); //W AND S
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
 
         Vector3 direction = new Vector3(-vertical, 0f, horizontal).normalized;
 
-        controller.Move(direction * speed * Time.deltaTime);
-
-        velocity.y += gravity * Time.deltaTime;
-
-        controller.Move(velocity * Time.deltaTime);
+        controller.Move(direction.normalized * speed * Time.deltaTime);
     }
 }
