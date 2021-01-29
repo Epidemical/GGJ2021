8af1f0e3cb777f15cfa2f9c9f5404c3f04704af7
Laura <51978253+Epidemical@users.noreply.github.com>
2021-01-29 22:16:56 +1000

    player falls

--- Assets/Scripts/CharacterMovemnet.cs
+++ Assets/Scripts/CharacterMovemnet.cs
@@ -6,16 +6,37 @@ public class CharacterMovemnet : MonoBehaviour
 {
 
     public CharacterController controller;
-    public float speed = 5f;
+    public Transform groundCheck;
+    public float groundDistance = 0.4f;
+    public LayerMask groundMask;
+
+    public float speed = 7f;
+    public float gravity = -9.81f;
+
+    Vector3 velocity;
+    bool isGrounded;
+
+
 
     // Update is called once per frame
     void Update()
     {
-        float horizontal = Input.GetAxisRaw("Horizontal");
-        float vertical = Input.GetAxisRaw("Vertical");
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+
+        if(isGrounded && velocity.y < 0)
+        {
+            velocity.y = -2f;
+        }
+
+        float horizontal = Input.GetAxisRaw("Horizontal"); //A AND D
+        float vertical = Input.GetAxisRaw("Vertical"); //W AND S
 
         Vector3 direction = new Vector3(-vertical, 0f, horizontal).normalized;
 
-        controller.Move(direction.normalized * speed * Time.deltaTime);
+        controller.Move(direction * speed * Time.deltaTime);
+
+        velocity.y += gravity * Time.deltaTime;
+
+        controller.Move(velocity * Time.deltaTime);
     }
 }
