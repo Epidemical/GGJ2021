cbce2a06643e4836f134a7ac54463fff307efdef
Genericmaile <thomasjlamb00@gmail.com>
2021-01-31 10:01:37 +1000

    re worked sounds (included world music again?)

--- Assets/Scripts/_SoundScript/AudioManager.cs
+++ Assets/Scripts/_SoundScript/AudioManager.cs
@@ -47,6 +47,12 @@ public class AudioManager : MonoBehaviour
     public void Play(string name)
     {
         Sound s = Array.Find(sounds, sound => sound.name == name);
+
+        if(s== null)
+        {
+            Debug.LogWarning("Warning, Audio File" + name + " Cannot be found ");
+            return;
+        }
         s.source.Play();
     }
 
